0ab6d92f1f2d1891180bc701c22e69dc13b8b1a4
Lau36 <95262628+Lau36@users.noreply.github.com>
2022-06-07 19:21:55 -0500

    Update tiempo_proceso.c

--- tiempo_proceso.c
+++ tiempo_proceso.c
@@ -1,6 +1,12 @@
+#include <stdio.h>
+#include <tiem.h>
+#include <unistd.h>
+
+int main(){
 time_t start=time();
 pid_t pid = fork();
 if (pid == 0) {
   time_t end = time();
   printf("%d\n", end - start);
+  }
 }
