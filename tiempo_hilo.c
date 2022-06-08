b95293af826f193edd39c8dc40ba71221c382bd5
Lau36 <95262628+Lau36@users.noreply.github.com>
2022-06-07 19:29:38 -0500

    Update tiempo_hilo.c

--- tiempo_hilo.c
+++ tiempo_hilo.c
@@ -1,3 +1,6 @@
+#include <stdio.h>                                                             
+#include <pthread.h>                                                           
+#include <time.h>  
 time_t start;
 
 void *worker(void *data) {
@@ -5,7 +8,9 @@ void *worker(void *data) {
   printf("%d\n", end - start);
 }
 
-int main() {
+int main(){
    start=time();
-   pthread_create(thread_id,NULL, worker,NULL);
+   pthread_t thread_id;
+   pthread_create(&thread_id,NULL, &worker,NULL);
+   pthread_join(thread_id,NULL);
 }
