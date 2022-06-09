27fb6627ca0c71357aa3c9ce85cba64d362f3e03
Lau36 <95262628+Lau36@users.noreply.github.com>
2022-06-09 12:06:09 -0500

    Update tiempo_hilo.c

--- tiempo_hilo.c
+++ tiempo_hilo.c
@@ -1,18 +1,19 @@
-#include <stdio.h>                                                             
-#include <pthread.h>                                                           
-#include <time.h>  
+//Tiempo de creacion de un hilo
+#include <stdio.h>
+#include <pthread.h>
+#include <time.h>
 clock_t start;
 
 void *worker(void *data) {
   clock_t end = clock();
-  printf("%ld\n", end - start);
+  printf("%ld\n", end - start);;
 }
 
 int main(){
+         printf("Tiempo:\n");
    start=clock();
    pthread_t thread; //identificador del hilo
-   pthread_create(&thread, NULL, &worker, NULL); //crea el hilo 
+   pthread_create(&thread, NULL, &worker, NULL); //crea el hilo
    pthread_join(thread, NULL);
-   printf("Fin\n");
-}
 
+}
