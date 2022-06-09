03ab3c2eaccef3ac14a2d67b481481edbfe4c677
Lau36 <95262628+Lau36@users.noreply.github.com>
2022-06-09 11:49:49 -0500

    Update tiempo_proceso.c

--- tiempo_proceso.c
+++ tiempo_proceso.c
@@ -1,12 +1,13 @@
+//El tiempo que se demora un proceso
 #include <stdio.h>
-#include <tiem.h>
+#include <time.h>
 #include <unistd.h>
 
 int main(){
-clock_t start;
-pid_t pid = fork();
-if (pid == 0) {
-  clock_t end = clock();
-  printf("%ld\n", end - start);
-  }
+        clock_t start;
+        pid_t pid = fork();
+        if (pid == 0) {
+         clock_t end = clock();
+        printf("%ld\n", end - start);
+        }
 }
